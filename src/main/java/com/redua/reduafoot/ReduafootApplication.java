e463bf406d65c9240b12cb95db0ef51b22f5e102
Joney Pereira <joney.pereira.ext@conductor.com.br>
2023-01-16 17:12:14 -0300

    Clone do projeto base

---
+++ src/main/java/com/redua/reduafoot/ReduafootApplication.java
@@ -0,0 +1,13 @@
+package com.redua.reduafoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReduafootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReduafootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redua/reduafoot/ReduafootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redua.reduafoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReduafootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
